45986c65b326ee7515ac0fac11371f4fbe103a5f
minjeong <hn01472@gmail.com>
2023-10-19 19:20:36 +0900

    CRD 구현 완료

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>WEB</h2>
+      Hello, WEB!!
+      <p>
+        <img src="/image.jpg" alt="image" width="100" />
+      </p>
+    </div>
+  );
+}
